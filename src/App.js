af8a7a07e9d510bc989b7a974a3eed08e0c447c2
canan <e.cananakin@gmail.com>
2020-07-30 13:50:12 +0100

    added some icons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import reactJs from './images/react-icon.svg'
+import html5 from './images/html5-icon.svg'
+import css3 from './images/css-icon.svg'
+import nodeJs from './images/nodejs-icon.svg'
+import mongoDB from './images/mongodb-icon.svg'
+import heroku from './images/heroku-icon.svg'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Welcome to Kodflix</h1>
-        </p>
-        
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<br/> <br/> <br/>
+			<div className="container">
+				<div className="item">
+					<img src={reactJs} alt="ReactJS"/>
+				</div>
+				<div className="item">
+					<img src={html5} alt="Html5"/>
+				</div>
+				<div className="item">
+					<img src={css3} alt="CSS3"/>
+				</div>
+			</div>
+			<div className="container">
+				<div className="item">
+					<img src={nodeJs} alt="NodeJs"/>
+				</div>
+				<div className="item">
+					<img src={mongoDB} alt="MongoDB"/>
+				</div>
+				<div className="item">
+					<img src={heroku} alt="Heroku"/>
+				</div>
+			</div>
+        </div>
+	);
 }
 
 export default App;
