0f61dddb5c0f365e83d3c70327828f015f2c7efa
canan <e.cananakin@gmail.com>
2020-08-01 11:13:31 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+
 import './App.css';
 import Technologies from './Technologies';
 import Detail from './Details/Detail'
