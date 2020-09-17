07e00e7e1f2ea6275dd6399580aa5bc7118abfcc
canan <e.cananakin@gmail.com>
2020-09-17 11:47:09 +0100

    edit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+  
+app.use(express.static(__dirname + '/dist/kodflix'));
+
+app.get('/*', function (req, res) {
+    res.sendFile(__dirname + '/dist/kodflix/index.html');
+});
+
+app.use(cors());
+app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
